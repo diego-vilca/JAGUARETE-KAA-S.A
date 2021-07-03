9ef708d223963f86e81d78b83309dcaebd4e8200
diego vilca <vicv87.mp@gmail.com>
2021-07-03 02:20:16 -0300

    estructura layout creada

--- PROYECTO/PROYECTO/urls.py
+++ PROYECTO/PROYECTO/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("SITIO.urls")),
     path('SITIO/', include("SITIO.urls")),
 ]
--- PROYECTO/SITIO/urls.py
+++ PROYECTO/SITIO/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls import path
 from . import views
 
+app_name = "SITIO"
 urlpatterns = [
     path('', views.index, name="index"),
 ]
